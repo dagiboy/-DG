7c19940d8905de4dd8c519109323619f5080735b
dagiboy <is>
2018-12-05 22:01:54 +0300

    Задание для Купчика.

--- Source/DG/CommonFunc.hpp
+++ Source/DG/CommonFunc.hpp
@@ -1,3 +1,9 @@
 #pragma once
 
 #include "DGApi.hpp"
+
+#include <list>
+#include <vector>
+
+// Та же функция, которую ты реализовывал в математике
+std::list<std::vector<int>> GetPossiblePerm(double **matr, int size);
