23ba19603b3218e0adb0f6471400330eb49bb397
dagiboy <is>
2018-12-07 00:00:37 +0300

    Добавил реализацию функций Minmax и GetBitMatr.

---
+++ Source/DG/CommonFunc.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+
+#include "CommonFunc.hpp"
+
+#include <list>
+#include <vector>
+#include "Matrix.hpp"
+
+Matrix GetBitMatrix(const Matrix &matr, double b)
+{
+	int size = matr.GetSize();
+	Matrix result(size);
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+			result[i][j] = matr[i][j] >= b ? 1 : 0;
+	}
+
+	return result;
+}
+
+double Minmax(const Matrix& matr)
+{
+	int size = matr.GetSize();
+	double maxInStr;
+	double minmax = DBL_MAX;
+
+	for (int i = 0; i < size; ++i)
+	{
+		maxInStr = matr[i][0];
+		for (int j = 0; j < size; ++j)
+		{
+			if (matr[i][j] > maxInStr)
+				maxInStr = matr[i][j];
+		}
+
+		if (maxInStr < minmax)
+			minmax = maxInStr;
+	}
+	
+	return minmax;
+}
--- Source/DG/CommonFunc.hpp
+++ Source/DG/CommonFunc.hpp
@@ -2,8 +2,12 @@
 
 #include "DGApi.hpp"
 
-#include <list>
-#include <vector>
+// На вход функция получает матрицу, состоящую из 0 и 1.
+// Функция должна вернуть список перестановок, матрицы которых меньше или равна входной матрице.
+DG_API std::list<std::vector<int>> GetPossiblePerm(const Matrix& matr);
 
-// Та же функция, которую ты реализовывал в математике
-std::list<std::vector<int>> GetPossiblePerm(double **matr, int size);
+// Возращает матрицу, в которой элемент равен 0, если соответсвующий ему элемент в матрице matr меньше b, 1 в противном случае.
+DG_API Matrix GetBitMatrix(const Matrix& matr, double b);
+
+// Возращает minmax матрицы.
+DG_API double Minmax(const Matrix& matr);
